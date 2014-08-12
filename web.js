0db33d41c5a94341e4986ea1cf098cce0d012eb8
Naveen <nvn.adarsh88@gmail.com>
2014-08-12 23:39:01 +0530

    Added Site Landing Page and a dummy sub-page

--- web.js
+++ web.js
@@ -2,14 +2,19 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var path = require('path');
 
+var oneDay = 86400000;
+
+app.use(express.static(__dirname + '/public'));
+app.use('/hotspots', express.static(__dirname + '/public/hotspots/app', {maxAge: oneDay}));
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('Hello World! nVn is building this page. will add on everything as time passes!');
-});
+//app.get('/', function(req, res) {
+//    res.send('Hello World! nVn is building this page. will add on everything as time passes!');
+//});
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
