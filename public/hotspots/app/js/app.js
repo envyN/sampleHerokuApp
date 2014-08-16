a2de21bede5df1063aee23d3b23e872603e9f194
Naveen <nvn.adarsh88@gmail.com>
2014-08-16 13:00:38 +0530

    Initial Template for Hotspots App

--- public/hotspots/app/js/app.js
+++ public/hotspots/app/js/app.js
@@ -2,15 +2,59 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module(
+        'hotspot', [
+            'ui.router',
+            'hotspot.filters',
+            'hotspot.services',
+            'hotspot.directives',
+            'hotspot.controllers'
+        ])
+        .config(function($stateProvider, $urlRouterProvider) {
+            $urlRouterProvider.otherwise("/");
+
+            $stateProvider
+                    .state('home', {
+                        url: "/",
+                        views: {
+                            'rootView': {
+                                templateUrl: './partials/landingPage.html',
+                                controller: 'homePageCtrl'
+                            }
+                        }
+                    })
+                    .state('about', {
+                        url: "/about",
+                        views: {
+                            'rootView': {
+                                templateUrl: './partials/about.html',
+                                controller: function($scope) {
+
+                                }//homePageCtrl
+                            }
+                        }
+                    })
+                    .state('contact', {
+                        url: "/contact",
+                        views: {
+                            'rootView': {
+                                templateUrl: './partials/landingPage.html',
+                                controller: function($scope) {
+
+                                }//homePageCtrl
+                            }
+                        }
+                    })
+                    .state('compare', {
+                        url: "/compare",
+                        views: {
+                            'rootView': {
+                                templateUrl: './partials/compare.html',
+                                controller: 'compareCtrl'
+                            }
+                        }
+                    });
+////        $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+////        $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+////        $routeProvider.otherwise({redirectTo: '/view1'});
+        });
--- public/hotspots/app/js/controllers.js
+++ public/hotspots/app/js/controllers.js
@@ -2,10 +2,18 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('hotspot.controllers', [])
+        .controller('homePageCtrl', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+            }])
+        .controller('compareCtrl', ['$scope', function($scope) {
+                $scope.sites = [
+                    {name: 'Shimla', popularity: 'High Popularity'},
+                    {name: 'Mussoorie', popularity: 'High Popularity'},
+                    {name: 'Korba', popularity: 'Low Popularity'},
+                    {name: 'Nainital', popularity: 'Low Popularity'},
+                    {name: 'Leh', popularity: 'High Popularity'},
+                    {name: 'McLeod Ganj', popularity: 'Low Popularity'}
+                ];
+                
+            }]);
